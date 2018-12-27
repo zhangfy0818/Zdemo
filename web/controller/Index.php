9806b714bea5b361407f10b07253ea8eec55c9a8
zhangfayuan <1137494466@qq.com>
2018-12-27 16:37:17 +0800

    新增Web控制器

---
+++ web/controller/Index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/12/27 0027
+ * Time: 16:26
+ */
+namespace web\controller;
+class Index{
+	public function show()
+	{
+		echo "Zdemo.....";
+	}
+}
